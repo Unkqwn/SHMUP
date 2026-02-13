f1aeebfbdf652ed40b81effe9190a07fe5387379
Unkqwn <Unkqwn@hotmail.com>
2026-02-13 14:49:51 +0100

    PlayerHPBar number indication added

--- Assets/Scripts/Player/Health/PlayerHPBar.cs
+++ Assets/Scripts/Player/Health/PlayerHPBar.cs
@@ -1,8 +1,10 @@
+using TMPro;
 using UnityEngine;
 
 public class PlayerHPBar : MonoBehaviour
 {
     [SerializeField] private RectTransform hpBar;
+    [SerializeField] private TextMeshProUGUI hpText;
 
     public void SetHealth(float currentHealth)
     {
@@ -13,5 +15,10 @@ public class PlayerHPBar : MonoBehaviour
 
         float healthPercent = currentHealth / 100f;
         hpBar.localScale = new Vector3(healthPercent, 1f, 1f);
+
+        if (hpText != null)
+        {
+            hpText.text = $"{currentHealth:0.00} / 100";
+        }
     }
 }
