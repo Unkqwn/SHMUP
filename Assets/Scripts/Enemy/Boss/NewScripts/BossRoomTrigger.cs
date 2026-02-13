c366c30c74148a905fbcabe4903c1e6d781f5c67
Unkqwn <Unkqwn@hotmail.com>
2026-02-13 23:43:27 +0100

    Boss room trigger added

---
+++ Assets/Scripts/Enemy/Boss/NewScripts/BossRoomTrigger.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using Cinemachine;
+
+public class BossRoomTrigger : MonoBehaviour
+{
+    [Header("Boss Settings")]
+    [SerializeField] private GameObject bossPrefab;
+    [SerializeField] private GameObject hpBar;
+
+    [Header("Camera Settings")]
+    [SerializeField] private CinemachineVirtualCamera bossCamera;
+    private int highPriority = 20;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            bossCamera.Priority = highPriority;
+            SpawnBoss();
+            DisableScrolling(other);
+        }
+        else if (other.CompareTag("Enemy"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+
+    private void SpawnBoss()
+    {
+        EnableBossHPBar();
+        GameObject boss = Instantiate(bossPrefab, transform.position, Quaternion.identity);
+    }
+
+    private void EnableBossHPBar()
+    {
+        hpBar.SetActive(true);
+    }
+
+    private void DisableScrolling(Collider player)
+    {
+        player.GetComponentInParent<Scrolling>().enabled = false;
+        player.GetComponentInParent<EnemySpawning>().enabled = false;
+    }
+}
